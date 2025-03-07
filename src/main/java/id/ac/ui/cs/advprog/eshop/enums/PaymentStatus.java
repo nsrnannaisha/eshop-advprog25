8d104122ec71ce708599710832983b30d19eb66e
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-03-07 17:31:12 +0700

    [REFACTOR] Add PaymentStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentStatus.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+    SUCCESS("SUCCESS"),
+    REJECTED("REJECTED"),
+    PENDING("PENDING");
+
+    private final String value;
+
+    PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (PaymentStatus status : PaymentStatus.values()) {
+            if (status.name().equals(param)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
