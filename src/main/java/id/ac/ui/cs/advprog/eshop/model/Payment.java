8f00e50d6630399f59f92e58121ec46185df1ae6
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-03-07 17:14:23 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,6 +2,8 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Arrays;
 import java.util.Map;
 
 @Getter
@@ -14,8 +16,49 @@ public class Payment {
     private String status;
 
     public Payment(String paymentId, Order order, String method, Map<String, String> paymentData) {
+        this.paymentId = paymentId;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        validatePayment();
+    }
+
+    private void validatePayment() {
+        switch (method) {
+            case "VOUCHER":
+                validateVoucher();
+                break;
+            case "BANK":
+                validateBankTransfer();
+                break;
+            default:
+                this.status = "REJECTED";
+        }
+    }
+
+    private void validateVoucher() {
+        String voucherCode = paymentData.get("voucherCode");
+        if (voucherCode != null && voucherCode.length() == 16 &&
+                voucherCode.startsWith("ESHOP") &&
+                voucherCode.replaceAll("[^0-9]", "").length() == 8) {
+            this.status = "SUCCESS";
+        } else {
+            this.status = "REJECTED";
+        }
+    }
+
+    private void validateBankTransfer() {
+        String bankName = paymentData.get("bankName");
+        String referenceCode = paymentData.get("referenceCode");
+        this.status = (bankName != null && !bankName.isEmpty() &&
+                referenceCode != null && !referenceCode.isEmpty()) ? "SUCCESS" : "REJECTED";
     }
 
     public void setStatus(String status) {
+        String[] statusList = {"REJECTED", "SUCCESS", "PENDING"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        }
+        this.status = status;
     }
 }
